fb3ab0cb910ee33481c47b2a3adae476095d5d3f
dayna_l <dayna.lashin@gmail.com>
2021-04-07 00:57:08 -0400

    Updated to print all results from page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 #scraping the website
 url = "https://www.setlist.fm/search?artist=53d69b79&query=The+National"
 r = requests.get(url)
@@ -17,16 +18,30 @@ results = soup.find("div", class_="main")
 column = results.find("div", class_="rightColumn")
 #print(column)
 content_box = column.find("div", class_="contentBox")
-setlist_preview = content_box.find("div", class_="setlistPreview")
-date_block = setlist_preview.find("div", class_="dateBlock")
-#print(date_block)
-month = date_block.find("span", class_="month")
-day = date_block.find("span", class_="day")
-year = date_block.find("span", class_="year")
+#print(content_box)
+setlist_preview = content_box.find_all("div", class_="setlistPreview")
+#print(len(setlist_preview))
+#print(setlist_preview)
 
-month_text = month.text
-day_text = day.text
-year_text = year.text
 
-print(f"The National's last show was: {month_text} {day_text}, {year_text}")
+#loops through each show setlist on page
+for set_list in setlist_preview:
+  #pulls out the date
+  date_block = set_list.find("div", "dateBlock")
+  month = date_block.find("span", class_="month").text
+  day = date_block.find("span", class_="day").text
+  year = date_block.find("span", class_="year").text
+
+  #pulls out the artist, venue, and location
+  title = set_list.find("h2")
+  title_text = title.find("a").text
+  title_edit = title_text.split(",")
+  artist = title_edit[0]
+  artist_edit = artist.split(" at ")
+  new_artist = artist_edit[0]
+  venue = artist_edit[1]
+  city = title_edit[1]
+  country = title_edit[2]
+
+  print(f"{new_artist} played {venue} in {city}, {country} on {month} {day}, {year}")
 
